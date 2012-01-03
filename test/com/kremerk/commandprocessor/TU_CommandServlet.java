bbddfcd5756c816b751b114118ac259ff547f181
Nick Kremer <njkremer@gmail.com>
2012-01-02 19:21:26 -0600

    Added in mockito for testing. Added in stub methods for tests.

--- test/com/kremerk/commandprocessor/TU_CommandServlet.java
+++ test/com/kremerk/commandprocessor/TU_CommandServlet.java
@@ -1,5 +1,37 @@
 package com.kremerk.commandprocessor;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class TU_CommandServlet {
 
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	public void testHandlingACommandWithNoParameters() {
+		
+	}
+	
+	@Test
+	public void testHandlingACommandWithParameters() {
+		
+	}
+	
+	@Test
+	public void testHandlingAMockCommmand() {
+		
+	}
+	
+	@Test
+	public void testHandlingACommandWithNoTypeSpecified() {
+		
+	}
+	
+	@Test
+	public void testHandlingACommandWithAnInvalidTypeSpecified() {
+		
+	}
 }
