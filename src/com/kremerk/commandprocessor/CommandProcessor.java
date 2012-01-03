00bce761b5b23f62413a83bdd62d9e986dede14c
Nick Kremer <njkremer@gmail.com>
2012-01-02 19:10:28 -0600

    removing in attemt to fix case…

--- src/com/kremerk/commandprocessor/CommandProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.kremerk.commandprocessor;
-
-import com.kremerk.commandprocessor.exception.CommandProcessorException;
-
-
-public interface CommandProcessor {
-	public Object processCommand(String commandSetName, String commandName, String... parameters) throws CommandProcessorException ;
-}
--- src/com/kremerk/commandprocessor/response/Response.java
+++
@@ -1,6 +0,0 @@
-package com.kremerk.commandprocessor.response;
-
-public interface Response {
-	public String getResponse();
-	public String getResponseType();
-}
