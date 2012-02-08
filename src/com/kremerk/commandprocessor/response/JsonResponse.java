4c60ae24b0b40bac0e8f4b02f697647cb2b5ecec
Nick Kremer <njkremer@gmail.com>
2012-02-07 22:22:59 -0600

    Changed tabs to spaces.

--- src/com/kremerk/commandprocessor/response/JsonResponse.java
+++ src/com/kremerk/commandprocessor/response/JsonResponse.java
@@ -3,26 +3,26 @@ package com.kremerk.commandprocessor.response;
 import org.json.JSONArray;
 
 public class JsonResponse implements Response {
-	public JsonResponse(JSONArray data) {
-		this.data = data;
-	}
-	
-	public JsonResponse(int status, String message, JSONArray data) {
-		this.message = message;
-		this.status = status;
-		this.data = data;
-	}
-	
-	public String getResponse() {
-		return String.format(template, status, message, data.toString());
-	}
-	
-	public String getContentType() {
-		return "text/javascript";
-	}
-	
-	private JSONArray data;
-	private String message = "Status OK";
-	private int status = 0;
-	private final static String template = "{\"status\":%s, \"message\":\"%s\", \"data\":%s}";
+    public JsonResponse(JSONArray data) {
+        this.data = data;
+    }
+
+    public JsonResponse(int status, String message, JSONArray data) {
+        this.message = message;
+        this.status = status;
+        this.data = data;
+    }
+
+    public String getResponse() {
+        return String.format(template, status, message, data.toString());
+    }
+
+    public String getContentType() {
+        return "text/javascript";
+    }
+
+    private JSONArray data;
+    private String message = "Status OK";
+    private int status = 0;
+    private final static String template = "{\"status\":%s, \"message\":\"%s\", \"data\":%s}";
 }
